df98d4fb714056ff9e24d8f5ab70b70c1f1f1cf7
Gerald <tank7575@yahoo.com>
2019-04-18 13:58:22 -0500

    first commit

---
+++ wordcount/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request):
+
+    return render(request, "home.html")
+
+def count(request):
+    return render(request, 'count.html')
